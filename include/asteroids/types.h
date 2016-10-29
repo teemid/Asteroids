3109e85f3c5623401f63d9ed6709817e9863c8cb
Emil Grunt <emilgrunt@gmail.com>
2016-10-29 21:04:19 +0200

    Adds a Size type to types.h

--- include/asteroids/types.h
+++ include/asteroids/types.h
@@ -3,18 +3,20 @@
 
 #include <cstdint>
 
-typedef uint8_t UInt8;
+typedef uint8_t  UInt8;
 typedef uint16_t UInt16;
 typedef uint32_t UInt32;
 typedef uint64_t UInt64;
 
-typedef int8_t Int8;
+typedef int8_t  Int8;
 typedef int16_t Int16;
 typedef int32_t Int32;
 typedef int64_t Int64;
 
 typedef float Real32;
 typedef float Real64;
+
+typedef UInt64 Size;
 
 typedef int32_t Bool32;
 
