5f50bf2f6d7f3f73ad84470129c21c8280d33267
Emil Grunt <emilgrunt@gmail.com>
2016-10-31 23:23:44 +0100

    Adds to the pool implementation

--- include/asteroids/pool.h
+++ include/asteroids/pool.h
@@ -20,16 +20,39 @@ namespace asteroids
         T * end (void);
     private:
         T elements[N];
+        T * free_list[N];
+        T * current;
         Size capacity;
     };
 
     template<typename T, UInt32 N>
-    Pool<T, N>::Pool (void) : capacity(N) { }
+    Pool<T, N>::Pool (void)
+    {
+        this->capacity = N;
+        
+        for (UInt32 i = 0; i < N; i++)
+        {
+            this->free_list[i] = elements + (N - i);
+        }
+
+        this->current = this->free_list[N];
+    }
 
     template<typename T, UInt32 N>
     T * Pool<T, N>::Get (void)
     {
+        T * free = this->current;
+        this->current = nullptr;
+        this->current = this->current - 1;
 
+        return free;
+    }
+
+    template<typename T, UInt32 N>
+    void Free (T * element)
+    {
+        this->current = this->current + 1;
+        this->current = element;
     }
 
     template<typename T, UInt32 N>
