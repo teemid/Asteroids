8446724fa3b612eea251bec3fca326c75e6dcf02
Emil Grunt <emilgrunt@gmail.com>
2016-11-01 22:36:02 +0100

    Fixes misplaced include

--- include/asteroids/memory.h
+++ include/asteroids/memory.h
@@ -1,9 +1,11 @@
 #ifndef ASTEROIDS_MEMORY_H
 #define ASTEROIDS_MEMORY_H
 
+#include <cstdlib>
+
 namespace asteroids
 {
-    #include <cstdlib>
+    
 
     #define ALLOCATE(type, bytes) (type *)malloc(bytes)
     #define REALLOCATE(type, old_memory, new_size) (type *)malloc(old_memory, new_size)
